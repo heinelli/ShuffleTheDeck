1b81b99d1e16b4cbd21be09b89058bdd1b7b8177
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-30 08:23:43 -0600

    Add random number generator function.

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,17 @@
+﻿Module ShuffleTheDeck
+
+    Sub Main()
+        Console.WriteLine(DrawCard())
+        Console.Read()
+    End Sub
+
+    Function DrawCard()
+        Dim suitValue As Integer
+        Dim numberValue As Integer
+        Randomize()
+        suitValue = Int((3 * Rnd()) + 1)
+        numberValue = Int((13 * Rnd()) + 1)
+        Return CInt(suitValue & numberValue)
+    End Function
+
+End Module
