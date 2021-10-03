262ca1fd79293d4c626eac9001bd759373fb958f
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-03 15:01:32 -0600

    Add if then statement and various functions.

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -10,59 +10,100 @@ Option Explicit On
 Module ShuffleTheDeck
 
     Sub Main()
-        Dim deck(12, 3) As Integer
+        Dim deck(12, 3) As Boolean
+        Dim value As Integer
+        Dim suit As Integer
+        Dim endStatement As Boolean
+
+
+        Do While endStatement = False
+            suit = CardSuit()
+            value = CardValue()
+
+            If deck(value, suit) = True Then
+                endStatement = False
+            Else
+                Console.WriteLine("Card drawn" & suit & "," & value)
+                deck(value, suit) = True
+                endStatement = False
+            End If
+            Console.ReadLine()
+        Loop
 
     End Sub
 
-    Function DrawCard()
+    Function CardSuit() As Integer
         Dim suitValue As Integer
-        Dim numberValue As Integer
-        Dim suitReturn As String
-        Dim numberReturn As String
+        Dim suitStringReturn As String
+        Dim suitIntegerReturn As Integer
 
         Randomize()
-        suitValue = Int((4 * Rnd()) + 0)
-        numberValue = Int((13 * Rnd()) + 0)
+        suitValue = CInt((4 * Rnd()) + 0)
 
         If suitValue = 0 Then
-            suitReturn = "Clubs"
+            suitStringReturn = "Clubs"
+            suitIntegerReturn = 0
         ElseIf suitValue = 1 Then
-            suitReturn = "Spades"
+            suitStringReturn = "Spades"
+            suitIntegerReturn = 1
         ElseIf suitValue = 2 Then
-            suitReturn = "Hearts"
+            suitStringReturn = "Hearts"
+            suitIntegerReturn = 2
         Else
-            suitReturn = "Diamonds"
+            suitStringReturn = "Diamonds"
+            suitIntegerReturn = 3
         End If
+        Return suitIntegerReturn
+    End Function
+
+    Function CardValue() As Integer
+        Dim numberValue As Integer
+        Dim valueReturn As Integer
+        Dim numberReturn As String
+
+        numberValue = CInt((13 * Rnd()) + 0)
 
         If numberValue = 0 Then
             numberReturn = "Ace"
+            valueReturn = 0
         ElseIf numberValue = 1 Then
             numberReturn = "2"
+            valueReturn = 1
         ElseIf numberValue = 2 Then
             numberReturn = "3"
+            valueReturn = 2
         ElseIf numberValue = 3 Then
             numberReturn = "4"
+            valueReturn = 3
         ElseIf numberValue = 4 Then
             numberReturn = "5"
+            valueReturn = 4
         ElseIf numberValue = 5 Then
             numberReturn = "6"
+            valueReturn = 5
         ElseIf numberValue = 6 Then
             numberReturn = "7"
+            valueReturn = 6
         ElseIf numberValue = 7 Then
             numberReturn = "8"
+            valueReturn = 7
         ElseIf numberValue = 8 Then
             numberReturn = "9"
+            valueReturn = 8
         ElseIf numberValue = 9 Then
             numberReturn = "10"
+            valueReturn = 9
         ElseIf numberValue = 10 Then
             numberReturn = "Jack"
+            valueReturn = 10
         ElseIf numberValue = 11 Then
             numberReturn = "Queen"
+            valueReturn = 11
         ElseIf numberValue = 12 Then
             numberReturn = "King"
+            valueReturn = 12
         End If
-
-        Return numberReturn & " of " & suitReturn
+        Return valueReturn
     End Function
 
 End Module
